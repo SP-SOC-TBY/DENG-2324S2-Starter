1138935a79f2627618d6306b11b26d7671724adb
tanby <tan_boon_yuen@ichat.sp.edu.sg>
2023-09-19 13:19:13 +0000

    added go to set up sql

--- starter/starter-setup.sql
+++ starter/starter-setup.sql
@@ -1,4 +1,5 @@
 create DATABASE test_db;
+go;
 
 USE test_db;
 CREATE TABLE test_tbl
@@ -6,9 +7,11 @@ CREATE TABLE test_tbl
 name varchar(100) not null,
 price decimal(7,2) not null, 
 primary key (id));
+go;
 
 INSERT INTO test_tbl
 (name, price)
 VALUES
 ('DBA', 500.00),  
 ('DFI',800.00);
+go;
